aeb721e236c5922fd6aaa36429be25481d4f36aa
dolqh1nslayer69 <happydolphin42@gmail.com>
2023-04-06 18:04:58 +0200

    Gescheite Linked List mit Unit Tests

---
+++ LinkedLists/LinkedList.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+
+namespace School.LinkedLists;
+
+public class LinkedList<T> : IList<T>
+{
+    private ListNode<T>? _head;
+    private ListNode<T>? _tail;
+    private ListNode<T>? _cur;
+    
+    public int Count { get; private set; }
+    public bool IsReadOnly { get; private set; } = false;
+
+    public T this[int index]
+    {
+        get
+        {
+            CheckIndexInBounds(index);
+            
+            var i = 0;
+            while (index > i)
+            {
+                _cur = _cur?.Next;
+                i++;
+            }
+
+            if (_cur is null) return default!;
+            
+            var temp = _cur.Data;
+            _cur = _head;
+            return temp;
+        }
+        set
+        {
+            CheckIndexInBounds(index);
+            
+            var i = 0;
+            while (index > i)
+            {
+                _cur = _cur?.Next;
+                i++;
+            }
+
+            _cur!.Data = value;
+            _cur = _head;
+        }
+    }
+
+    private void CheckIndexInBounds(int index)
+    {
+        if (index >= Count) throw new ArgumentOutOfRangeException(nameof(index), "0 to " + Count, "The Index was bigger then the number of elements in the List");
+        if (index < 0) throw new ArgumentOutOfRangeException(nameof(index), "0 to " + Count, "The Index was a negative number");
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        while (_cur is not null)
+        {
+            yield return _cur.Data;
+            _cur = _cur.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Add(T item)
+    {
+        if (_tail is not null)
+        {
+            _tail.Next = new ListNode<T>(item); 
+            _tail = _tail.Next;
+            Count++;
+            return;
+        }
+        _tail = new ListNode<T>(item);
+        _head = _tail;
+        _cur = _head;
+        Count++;
+    }
+
+    public void Clear()
+    {
+        _head = null;
+        _tail = null;
+        Count = 0;
+    }
+
+    public bool Contains(T item)
+    {
+        if (item is null) return false;
+        
+        while (_cur is not null)
+        {
+            if (_cur.Data!.Equals(item)) return true;
+        }
+
+        return false;
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        if (arrayIndex < 0) throw new ArgumentOutOfRangeException(nameof(arrayIndex), nameof(arrayIndex) + " is less than 0");
+        if (array is null) throw new ArgumentNullException(nameof(array), nameof(array) + " is null");
+        var i = 0;
+        while (_cur is not null)
+        {
+            if (i + arrayIndex > array.Length) throw new ArgumentException("The number of elements in the source ICollection<T> is greater than the available space from arrayIndex to the end of the destination array.");
+            array[arrayIndex + i] = _cur.Data;
+            _cur = _cur.Next;
+            i++;
+        }
+    }
+
+    public bool Remove(T item)
+    {
+        while (_cur.Next is not null)
+        {
+            if (_cur.Next.Data.Equals(item))
+            {
+                _cur.Next = _cur.Next.Next;
+                Count--;
+                return true;
+            }
+            _cur = _cur.Next;
+        }
+
+        return false;
+    }
+
+    public int IndexOf(T item)
+    {
+        var i = 0;
+        while (_cur is not null)
+        {
+            if (_cur.Data.Equals(item)) return i;
+            _cur = _cur.Next;
+            i++;
+        }
+
+        return -1;
+    }
+
+    public void Insert(int index, T item)
+    {
+        if (index >= Count || index < 0) throw new ArgumentOutOfRangeException(nameof(index));
+
+        if (index == 0)
+        {
+            _head = new ListNode<T>(item)
+            {
+                Next = _cur
+            };
+            _cur = _head;
+            Count++;
+            return;
+        }
+        
+        var i = 0;
+        while (i + 1 < index)
+        {
+            _cur = _cur.Next;
+            i++;
+        }
+
+        var temp = new ListNode<T>(item)
+        {
+            Next = _cur.Next
+        };
+        _cur.Next = temp;
+        _cur = _head;
+        Count++;
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index >= Count || index < 0) throw new ArgumentOutOfRangeException(nameof(index));
+
+        if (index == 0)
+        {
+            _head = _head.Next;
+            _cur = _head;
+            Count--;
+            return;
+        }
+        
+        var i = 0;
+        while (i + 1 < index)
+        {
+            _cur = _cur.Next;
+            i++;
+        }
+
+        _cur.Next = _cur.Next.Next;
+        Count--;
+    }
+
+    private class ListNode<TNode>
+    {
+        public TNode Data { get; set; }
+        public ListNode<TNode>? Next { get; set; }
+
+        public ListNode(TNode data)
+        {
+            Data = data;
+        }
+    }
+}
---
+++ Tests/LinkedListTests.cs
@@ -0,0 +1,416 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Tests;
+
+public class LinkedListTests
+{
+    [Theory]
+    [InlineData(5, 5, 0)]
+    [InlineData(12, 35, 15151515)]
+    [InlineData(24, 40, 03030)]
+    [InlineData(48, 69, 12409)]
+    [InlineData(128, 196, -1)]
+    public void ArrayAccess_ShouldGetTheItemAtSpecifiedIndex(int listSize, int checks, int seed)
+    {
+        // Arrange
+        IList<int> list = new School.LinkedLists.LinkedList<int>();
+        var reference = new int[listSize];
+        
+        var rnd = new Random(seed);
+
+        for (var i = 0; i < listSize; i++)
+        {
+            int item = rnd.Next(-9999, 9999);
+            list.Add(item);
+            reference[i] = item;
+        }
+        
+        // Act and Assert
+        for (int i = 0; i < checks; i++)
+        {
+            int index = rnd.Next(listSize);
+            list[index].Should().Be(reference[index]);
+        }
+    }
+
+    [Theory]
+    [InlineData(5, 0)]
+    [InlineData(12, 15151515)]
+    [InlineData(24, 03030)]
+    [InlineData(48, 12409)]
+    [InlineData(128, -1)]
+    [InlineData(1024, 2048)]
+    public void ArrayAccesSet_ShouldReplaceItemAtSpecifiedIndex(int listSize, int seed)
+    {
+        // Arrange
+        IList<int> list = new School.LinkedLists.LinkedList<int>();
+        int[] reference = new int[listSize];
+        
+        for (int i = 0; i < listSize; i++)
+        {
+            list.Add(int.MinValue);
+            reference[i] = int.MinValue;
+        }
+
+        var rnd = new Random(seed);
+        
+        // Act
+        for (int i = 0; i < listSize; i++)
+        {
+            int item = rnd.Next(-9999, 9999);
+            list[i] = item;
+            reference[i] = item;
+        }
+        
+        // Assert
+        for (int i = 0; i < listSize; i++)
+        {
+            list[i].Should().Be(reference[i]);
+        }
+    }
+
+    [Theory]
+    [InlineData(32, 33)]
+    [InlineData(1, 1)]
+    [InlineData(48, int.MaxValue)]
+    public void ArrayAccessGet_ShouldThrow_WhenIndexTooBig(int listSize, int index)
+    {
+        // Arrange
+        IList<int> list = new School.LinkedLists.LinkedList<int>();
+        for (int i = 0; i < listSize; i++)
+        {
+            list.Add(i);
+        }
+        
+        // Act and Assert
+        list.Invoking(l =>
+        {
+            int i = l[index];
+        }).Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Theory]
+    [InlineData(32, 33)]
+    [InlineData(1, 1)]
+    [InlineData(48, int.MaxValue)]
+    public void ArrayAccessSet_ShouldThrow_WhenIndexTooBig(int listSize, int index)
+    {
+        // Arrange
+        IList<int> list = new School.LinkedLists.LinkedList<int>();
+        for (int i = 0; i < listSize; i++)
+        {
+            list.Add(i);
+        }
+        
+        // Act and Assert
+        list.Invoking(l =>
+        {
+            return l[index] = 32;
+        }).Should().Throw<ArgumentOutOfRangeException>();
+    }
+    
+    [Theory]
+    [InlineData(32, -1)]
+    [InlineData(1, -9999)]
+    [InlineData(48, int.MinValue)]
+    public void ArrayAccessGet_ShouldThrow_WhenIndexLessThanZero(int listSize, int index)
+    {
+        // Arrange
+        IList<int> list = new School.LinkedLists.LinkedList<int>();
+        for (int i = 0; i < listSize; i++)
+        {
+            list.Add(i);
+        }
+        
+        // Act and Assert
+        list.Invoking(l =>
+        {
+            int i = l[index];
+        }).Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Theory]
+    [InlineData(32, -1)]
+    [InlineData(1, -9999)]
+    [InlineData(48, int.MinValue)]
+    public void ArrayAccessSet_ShouldThrow_WhenIndexLessThanZero(int listSize, int index)
+    {
+        // Arrange
+        IList<int> list = new School.LinkedLists.LinkedList<int>();
+        for (int i = 0; i < listSize; i++)
+        {
+            list.Add(i);
+        }
+        
+        // Act and Assert
+        list.Invoking(l =>
+        {
+            l[index] = 32;
+        }).Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Theory]
+    [InlineData()]
+    [InlineData(1)]
+    [InlineData(1,2,3)]
+    [InlineData(-1,0,1)]
+    [InlineData(1,1,1,1,1,1,1)]
+    [InlineData(-9999999, 0, -1234, 9999999)]
+    [InlineData(1,2,5,6,1,2,4,4,1,2,4,1,4,35,124,6,7,2,543,3,4,2,2,3,3,4,5,6,9388,12,4,1,2,2,1,0)]
+    public void Item_ShouldBeAddedToEndOfList_WhenAddIsCalled(params int[] items)
+    {
+        // Arrange
+        IList<int> list = new School.LinkedLists.LinkedList<int>();
+
+        // Act
+        foreach (int item in items)
+        {
+            list.Add(item);
+        }
+
+        // Assert
+        for (var i = 0; i < items.Length; i++)
+        {
+            list[i].Should().Be(items[i]);
+        }
+    }
+    
+    [Fact]
+    public void AddingNull_ShouldNotThrow()
+    {
+        // Arrange
+        IList<object> list = new School.LinkedLists.LinkedList<object>();
+        list.Add(-1);
+        list.Add(-1);
+        
+        // Act and Assert
+        list.Invoking(l =>
+        {
+            l.Add(null!);
+        }).Should().NotThrow();
+    }
+
+    [Theory]
+    [InlineData()]
+    [InlineData(1)]
+    [InlineData(1,2,3)]
+    [InlineData(-1,0,1)]
+    [InlineData(1,1,1,1,1,1,1)]
+    [InlineData(-9999999, 0, -1234, 9999999)]
+    [InlineData(1,2,5,6,1,2,4,4,1,2,4,1,4,35,124,6,7,2,543,3,4,2,2,3,3,4,5,6,9388,12,4,1,2,2,1,0)]
+    public void Count_ShouldBeUpdated_WhenItemIsAdded(params int[] items)
+    {
+        // Arrange
+        IList<int> list = new School.LinkedLists.LinkedList<int>();
+        
+        // Act
+        foreach (int item in items)
+        {
+            list.Add(item);
+        }
+        
+        // Assert
+        list.Count.Should().Be(items.Length);
+    }
+    
+    [Theory]
+    [InlineData()]
+    [InlineData(1)]
+    [InlineData(1,2,3)]
+    [InlineData(-1,0,1)]
+    [InlineData(1,1,1,1,1,1,1)]
+    [InlineData(-9999999, 0, -1234, 9999999)]
+    [InlineData(1,2,5,6,1,2,4,4,1,2,4,1,4,35,124,6,7,2,543,3,4,2,2,3,3,4,5,6,9388,12,4,1,2,2,1,0)]
+    public void Count_ShouldBeUpdated_WhenItemIsInserted(params int[] items)
+    {
+        // Arrange
+        IList<int> list = new School.LinkedLists.LinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+
+        // Act
+        foreach (int item in items)
+        {
+            list.Insert(1, item);
+        }
+        
+        // Assert
+        list.Count.Should().Be(items.Length + 2);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(1123123123)]
+    [InlineData(-1565656)]
+    [InlineData(-474)]
+    [InlineData(5656)]
+    [InlineData(0)]
+    [InlineData(32)]
+    public void Insert_InsertsElementAtSpecifiedIndex(int seed)
+    {
+        // Arrange
+        IList<int> list = new School.LinkedLists.LinkedList<int>();
+
+        var rnd = new Random(seed);
+        int numberOfInsertions = rnd.Next(128);
+
+        var items = new int[numberOfInsertions];
+        var indices = new int[numberOfInsertions];
+
+        for (var i = 0; i < numberOfInsertions; i++)
+        {
+            items[i] = rnd.Next(int.MinValue, int.MaxValue);
+            indices[i] = rnd.Next(1, i + 1);
+        }
+        
+        // Act and Assert
+        list.Add(0);
+        list.Add(1);
+
+        for (var i = 0; i < numberOfInsertions; i++)
+        {
+            list.Insert(indices[i], items[i]);
+            list[indices[i]].Should().Be(items[i]);
+        }
+    }
+
+    //TODO rewrite
+    [Fact]
+    public void Insert_ThrowsExceptionWhenIndexIsOutOfRange()
+    {
+        // Arrange
+        IList<int> list = new School.LinkedLists.LinkedList<int>();
+
+        // Act & Assert
+        list.Invoking(l => l.Insert(-1, 1)).Should().Throw<ArgumentOutOfRangeException>();
+        list.Invoking(l => l.Insert(2, 1)).Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void InsertingNull_ShouldNotThrow()
+    {
+        // Arrange
+        IList<object> list = new School.LinkedLists.LinkedList<object>();
+        list.Add(-1);
+        list.Add(-1);
+        
+        // Act and Assert
+        list.Invoking(l =>
+        {
+            l.Insert(1, null!);
+        }).Should().NotThrow();
+    }
+
+    // TODO rewrite
+    [Fact]
+    public void RemoveAt_RemovesElementAtSpecifiedIndex()
+    {
+        // Arrange
+        IList<int> list = new School.LinkedLists.LinkedList<int>();
+
+        // Act
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.RemoveAt(1);
+
+        // Assert
+        list.Should().HaveCount(2);
+        list[0].Should().Be(1);
+        list[1].Should().Be(3);
+    }
+
+    //TODO rewrite
+    [Fact]
+    public void RemoveAt_ThrowsExceptionWhenIndexIsOutOfRange()
+    {
+        // Arrange
+        IList<int> list = new School.LinkedLists.LinkedList<int>();
+
+        // Act & Assert
+        list.Invoking(l => l.RemoveAt(-1)).Should().Throw<ArgumentOutOfRangeException>();
+        list.Invoking(l => l.RemoveAt(1)).Should().Throw<ArgumentOutOfRangeException>();
+    }
+    
+    // TODO Remove() updates count
+
+    // todo rewrite
+    [Fact]
+    public void IndexOf_ReturnsIndexOfElement()
+    {
+        // Arrange
+        IList<int> list = new School.LinkedLists.LinkedList<int>();
+
+        // Act
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        int index = list.IndexOf(2);
+
+        // Assert
+        index.Should().Be(1);
+    }
+
+    // todo rewrite
+    [Fact]
+    public void IndexOf_ReturnsMinusOneWhenElementNotFound()
+    {
+        // Arrange
+        IList<int> list = new School.LinkedLists.LinkedList<int>();
+
+        // Act
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        int index = list.IndexOf(4);
+
+        // Assert
+        index.Should().Be(-1);
+    }
+
+    // todo rewrite
+    [Fact]
+    [SuppressMessage("ReSharper", "LoopCanBeConvertedToQuery")]
+    public void GetEnumerator_EnumeratesListCorrectly()
+    {
+        // Arrange
+        IList<int> list = new School.LinkedLists.LinkedList<int>();
+
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+
+        // Act
+        var values = new List<int>();
+        foreach (int item in list)
+        {
+            values.Add(item);
+        }
+
+        // Assert
+        values.Should().HaveCount(3);
+        values[0].Should().Be(1);
+        values[1].Should().Be(2);
+        values[2].Should().Be(3);
+    }
+
+    // todo rewrite
+    [Fact]
+    public void Clear_ClearsList()
+    {
+        // Arrange
+        IList<int> list = new School.LinkedLists.LinkedList<int>();
+
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+
+        // Act
+        list.Clear();
+
+        // Assert
+        list.Should().HaveCount(0);
+    }
+    
+    // todo check other generic types than integers
+}
